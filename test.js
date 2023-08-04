5162642ea2ccd1ded60532e5f022b95ea45a3860
PyaePhyo7522 <pyaephyoaung.may@gmaiiil.com>
2023-08-04 19:54:13 +0630

    Adding another file

--- test.js
+++ test.js
@@ -3,4 +3,4 @@ const fs = require("fs");
 fs.promises.readFile("./data.txt")
             .then(data => console.log("Promise Data: ", data.toString().length));
 
-console.log("END");
+console.log("END"); 
